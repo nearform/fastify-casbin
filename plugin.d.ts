483e9d8a1793fceb48a7ab114345bf2e6c5f0075
Igor Savin <iselwin@gmail.com>
2021-01-09 20:16:25 +0200

    Improve comment

--- plugin.d.ts
+++ plugin.d.ts
@@ -13,7 +13,7 @@ declare module 'fastify' {
 
 export interface FastifyCasbinOptions {
   modelPath?: string
-  enforcerParam?: any // could be anything that newEnforcer() supports as the first argument, including an instance of a casbin model initialized in code. Popular options include `string` for modelPath and `Model` for programmatically instantiated model.
+  enforcerParam?: any // could be anything that newEnforcer() supports as the first argument. Common cases include `string` for modelPath and `Model` for programmatically instantiated model.
   adapter: string | Adapter
   watcher?: Watcher
 }
